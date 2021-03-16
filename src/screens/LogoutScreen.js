5ea7937e6cb0581b6544dd85d8470a3742adc3c6
Dawson Ren <dawsonren@gmail.com>
2021-03-15 22:12:42 -0500

    Refactored basic layout, changed LogoutScren styles

---
+++ src/screens/LogoutScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Icon } from 'react-native-elements'
+
+export const LogoutScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Icon name='hands'
+                  type='font-awesome-5'></Icon>
+            <Text>Accord</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FFAEC0',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
